e13ead24b9f42819bb98ed5b3bad32097476d390
Robert Marinho <robstermarinho@gmail.com>
2019-09-08 16:25:34 -0300

    Async Storage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Keyboard, ActivityIndicator} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 import {
   Container,
   Form,
@@ -14,7 +16,6 @@ import {
   ProfileButton,
   ProfileButtonText,
 } from './styles';
-import api from '../../services/api';
 
 export default class Main extends Component {
   state = {
@@ -22,8 +23,29 @@ export default class Main extends Component {
     users: [],
     loading: false,
   };
+
+  /*
+  // Retrieve from storage
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({
+        users: JSON.parse(users),
+      });
+    }
+  }
+
+  // Update the storage
+  componentDidUpdate(_, prevState) {
+    const {users} = this.state;
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
+  */
+
   handleAddUser = async () => {
-    console.tron.log(this.state.newUser);
     const {users, newUser} = this.state;
     this.setState({loading: true});
     const response = await api.get(`/users/${newUser}`);
@@ -35,6 +57,7 @@ export default class Main extends Component {
       avatar: response.data.avatar_url,
     };
 
+
     this.setState({
       users: [...users, data],
       newUser: '',
@@ -45,6 +68,8 @@ export default class Main extends Component {
     Keyboard.dismiss();
   };
   render() {
+    console.tron.log(this.props);
+
     const {users, newUser, loading} = this.state;
     return (
       <Container>
